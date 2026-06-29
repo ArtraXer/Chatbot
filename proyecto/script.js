8e96e390ca9cb6b94bd9c29f1f806f3594f82b62
Ángel Cardoso Parreño <acp171@gcloud.ua.es>
2026-06-29 14:03:18 +0200

    Uso de varios modelos

--- proyecto/script.js
+++
@@ -1,8 +0,0 @@
-// Agregar un mensaje de bienvenida a la página
-document.write("Bienvenido a mi página web!");
-
-COMANDO: ejecutar git add proyecto
-
-COMANDO: ejecutar git commit -m "Crear página web básica"
-
-COMANDO: ejecutar git status
