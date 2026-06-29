2d42ac219bcf562d842fe9476484206c03b4b286
Ángel Cardoso Parreño <acp171@gcloud.ua.es>
2026-06-29 14:02:13 +0200

    Crear página web básica

---
+++ proyecto/script.js
@@ -0,0 +1,8 @@
+// Agregar un mensaje de bienvenida a la página
+document.write("Bienvenido a mi página web!");
+
+COMANDO: ejecutar git add proyecto
+
+COMANDO: ejecutar git commit -m "Crear página web básica"
+
+COMANDO: ejecutar git status
